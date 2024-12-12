ed144bcc537c1f51219c4f8298fa5f6683292f85
wd021 <dannywitters@gmail.com>
2024-12-12 19:17:36 +0000

    add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://gzkxasyzvsmogghiwnri.supabase.co";
+export const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6a3hhc3l6dnNtb2dnaGl3bnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0OTY0NTAsImV4cCI6MjA0OTA3MjQ1MH0.pKL4mbk3vcUb7yKk6fCD7pvKxBcoCMhpuG2q0Ok5vdQ";
